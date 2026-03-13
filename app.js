c75f7dccfc8ae7c4cde1e330533d2fe34810de86
Andrey Ortega Garcìa <240370@utxicotepec.edu.mx>
2026-03-13 14:35:47 -0600

    add: Se creó el servidor de node express

---
+++ app.js
@@ -0,0 +1,22 @@
+//Servidor
+const express = require('express');
+const logger = reuquire ('morgan');
+const bodyParser = require('body-parser');
+//Apis
+const http = require ('http');
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', (req, res) => res.status(200).send({
+   message: 'Bienvendido a mi API de tienda virtual',
+}));
+
+const port = parseInt(process.env.PORT, 10) || 8000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
+module.exports = app;
